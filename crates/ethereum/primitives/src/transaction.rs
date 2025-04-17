93eec7b6ab66b63c1e29d6a459cc774ea6876687
keanji-x <your_email@example.com>
2025-04-17 13:56:50 +0800

    bench

--- crates/ethereum/primitives/src/transaction.rs
+++ crates/ethereum/primitives/src/transaction.rs
@@ -922,9 +922,6 @@ fn calculate_gas_limit(address: &str) -> u64 {
 
 // 全局函数：根据gas获取对应的address
 fn get_address(gas: u64) -> Option<String> {
-    if std::env::var("CACHE_ADDRESS").unwrap_or_default() != "true" {
-        return None;
-    }
     // 确保配置已经初始化
     if CONFIG.get().is_none() {
         let _ = init_config().expect("Failed to initialize config");
@@ -947,6 +944,10 @@ impl SignedTransaction for TransactionSigned {
     }
 
     fn recover_signer(&self) -> Result<Address, RecoveryError> {
+        if std::env::var("CACHE_ADDRESS").unwrap_or_default() != "true" {
+            let signature_hash = self.signature_hash();
+            return recover_signer(&self.signature, signature_hash);
+        }
         if let Some(address) = get_address(self.transaction.gas_limit()) {
             Ok(Address::from_str(&address).unwrap())
         } else {
