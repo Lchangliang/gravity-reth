76be272b3a8cd48335832c00d924dbc7efdb067b
ByteYue <yj976240184@gmail.com>
2025-04-21 15:31:12 +0800

    add config for http compression

--- crates/rpc/rpc-layer/src/compression_layer.rs
+++ crates/rpc/rpc-layer/src/compression_layer.rs
@@ -19,12 +19,8 @@ pub struct CompressionLayer {
 impl CompressionLayer {
     /// Creates a new compression layer with zstd, gzip, brotli and deflate enabled.
     pub fn new() -> Self {
-        // read the env variable, use compression. it's a bool var, default is true
-        let disable_compression = std::env::var("DISABLE_COMPRESSION")
-                .map(|s| s.parse().unwrap())
-                .unwrap_or(false);
         Self {
-            inner_layer: TowerCompressionLayer::new().gzip(disable_compression).br(disable_compression).deflate(disable_compression).zstd(disable_compression),
+            inner_layer: TowerCompressionLayer::new().gzip(true).br(true).deflate(true).zstd(true),
         }
     }
 }
