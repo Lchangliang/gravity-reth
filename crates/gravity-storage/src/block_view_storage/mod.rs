5fc52828ffcffcd1abe42d3846959f4c11ac4a86
Lightman <915311741@qq.com>
2025-05-06 10:48:32 +0800

    tmp

--- crates/gravity-storage/src/block_view_storage/mod.rs
+++ crates/gravity-storage/src/block_view_storage/mod.rs
@@ -109,7 +109,7 @@ where
 
         let start_time = Instant::now();
         let mut input = TrieInput::default();
-        let revert_state = consistent_view.revert_state_with_block_number(base_block_hash, base_block_number).unwrap();
+        let revert_state = HashedPostState::default();
         input.append(revert_state);
 
         let (hashed_state_vec, trie_updates_vec) = {
